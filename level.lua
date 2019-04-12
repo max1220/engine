f5d6613bcfab85d494c2ed0debeeaf8ef477b790
max1220 <max1220@users.noreply.github.com>
2019-04-12 21:12:40 +0200

    tweaked debug level generator

--- level.lua
+++ level.lua
@@ -1,4 +1,9 @@
 local level = {
+	spawn_x = 20,
+	spawn_y = 20,
+	spawn_velocity_x = 20,
+	spawn_velocity_y = 20,
+
 	world_data = {
 		{
 			type = "collider",
@@ -7,27 +12,39 @@ local level = {
 			w = 50,
 			h = 10
 		},
+		
+		{
+			type = "collider",
+			x = 60,
+			y = 00,
+			w = 10,
+			h = 50
+		},
 
 		{
 			type = "collider",
 			x = 60,
 			y = 80,
-			w = 30,
+			w = 50,
 			h = 5
 		}
 
-	}
+	},
+	width = 0,
+	height = 100
 }
 local cx = 100
+local last_y = 80
 for i=1, 100 do
 	local rect = {
 		type = "collider"
 	}
 	rect.x = cx + math.random(1,30)
-	rect.y = 80 + math.random(-30, 30)
-	rect.w = math.random(10, 60)
+	rect.y = math.max(math.min(last_y + math.random(-10, 10), 100), 20)
+	rect.w = math.random(60, 160)
 	rect.h = math.random(5, 15)
 	cx = cx + rect.w
+	last_y = rect.y
 	table.insert(level.world_data, rect)
 end
 

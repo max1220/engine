67894f839fe9c1b228a1768b2f4834b0479427a1
max1220 <max1220@users.noreply.github.com>
2019-04-12 21:12:57 +0200

    Added shooting bullets to test the collision engine

--- game.lua
+++ game.lua
@@ -22,9 +22,46 @@ end
 
 -- draw the player on the drawbuffer
 local function draw_player(db)
-	local player_db = player.drawbuffers[player.state][1]
+	local player_db
+	if player.velocity_x == 0 then
+		if player.dir == "right" then
+			player_db = player.drawbuffers.standing_right[1]
+		elseif player.dir == "left" then
+			player_db = player.drawbuffers.standing_left[1]
+		end
+	else
+		if player.dir == "right" then
+			player_db = player.drawbuffers.walking_right[1]
+		elseif player.dir == "left" then
+			player_db = player.drawbuffers.walking_left[1]
+		end
+	end
 	local screen_x, screen_y = world_to_screen_coords(player.x, player.y)	
 	player_db:draw_to_drawbuffer(db, screen_x, screen_y, 0, 0, player_db:width(), player_db:height())
+end
+
+
+-- create a bullet from the player, towards x,y
+local bullets = {}
+local function player_shoot(dx,dy)
+	local bullet = {
+		x = player.x,
+		y = player.y + 5,
+		w = 1,
+		h = 1,
+		dx = dx,
+		dy = dy,
+		speed = 60
+	}
+	
+	if dx > 0 then
+		bullet.x = bullet.x + player.width + 5
+	else
+		bullet.x = bullet.x - 5
+	end
+	
+	world.physics_world:add(bullet, bullet.x, bullet.y, bullet.w, bullet.h)
+	table.insert(bullets, bullet)
 end
 
 
@@ -38,19 +75,20 @@ local function update_player(dt)
 	end
 	if engine:key_is_down(input.event_codes.KEY_LEFT) then
 		player.velocity_x = -player.speed_x
+		player.dir = "left"
 	end
 	if engine:key_is_down(input.event_codes.KEY_RIGHT) then
 		player.velocity_x = player.speed_x
+		player.dir = "right"
 	end
-	
-	if player.velocity_x > 1 then
-		player.state = "walking_right"
-	elseif player.velocity_x < -1 then
-		player.state = "walking_left"
-	elseif player.state == "walking_left" then
-		player.state = "standing_left"
-	elseif player.state == "walking_right" then
-		player.state = "standing_right"
+	if engine:key_is_down(input.event_codes.KEY_SPACE) then
+		if player.can_shoot then
+			if player.dir == "right" then
+				player_shoot(1, 0)
+			elseif player.dir == "right" then
+				player_shoot(-1, 0)
+			end
+		end
 	end
 	
 	-- Apply gravity
@@ -61,26 +99,61 @@ local function update_player(dt)
 		player.velocity_x = player.velocity_x * player.friction_ground
 	else
 		player.velocity_x = player.velocity_x * player.friction_air
-		player.velocity_y = player.velocity_y * player.friction_air
+		--player.velocity_y = player.velocity_y * player.friction_air
+	end
+	
+	if math.abs(player.velocity_x) < 0.01 then
+		player.velocity_x = 0
 	end
 	
 	if player.velocity_y ~= 0 then
 		player.is_on_ground = false
 	end
 	if player.velocity_x ~= 0 or player.velocity_y ~= 0 then
-		local cols
-		player.x, player.y, cols, cols_len = world.physics_world:move(player, player.x + player.velocity_x * dt, player.y + player.velocity_y * dt)
+		local cols, cols_len
+		player.x, player.y, cols, cols_len = world.physics_world:move(player, player.x + player.velocity_x * dt, player.y + player.velocity_y * dt, colission_filter)
 		for i=1, cols_len do
 			local col = cols[i]
-			player.is_on_ground = true
-			player.velocity_y = 0
-			print(("col.other = %s, col.type = %s, col.normal = %d,%d"):format(col.other, col.type, col.normal.x, col.normal.y))
+			if col.normal.y ~= 0 then			
+				player.is_on_ground = true
+				player.velocity_y = 0
+			end
+			if col.normal.x ~= 0 then
+				player.velocity_x = 0
+			end
+			
+			-- print(("col.other = %s, col.type = %s, col.normal = %d,%d"):format(col.other, col.type, col.normal.x, col.normal.y))
 		end
 	end
 	
 	
 end
 
+
+-- update bullet positions, handle collisions
+local function update_bullets(dt)
+	for i, bullet in ipairs(bullets) do
+		local cols, cols_len
+		bullet.x, bullet.y, cols, cols_len = world.physics_world:move(bullet, bullet.x + bullet.dx*bullet.speed*dt, bullet.y + bullet.dy*bullet.speed*dt, colission_filter)
+		for j=1, cols_len do
+			local col = cols[j]
+			player.can_shoot = true
+			table.remove(bullets, i)
+			world.physics_world:remove(bullet)
+			return
+		end
+	end
+end
+
+
+local function draw_bullets(db)
+	for i, bullet in ipairs(bullets) do
+		local screen_x, screen_y = world_to_screen_coords(bullet.x, bullet.y)
+		local last_x, last_y = world_to_screen_coords(bullet.x - bullet.dx*bullet.speed*0.1, bullet.y - bullet.dy*bullet.speed*0.1)
+		db:set_line(screen_x, screen_y, last_x, last_y, 0,0,255,255)
+		-- db:set_pixel(screen_x, screen_y, 0,0,255,255)
+	end
+end
 
 
 -- called when the calculations should be done
@@ -89,12 +162,12 @@ function game:update(dt)
 	update_player(dt)
 	scroll_x = -(player.x) + (width/2)
 	
-	if self:key_is_down(input.event_codes.KEY_A) then
-		scroll_x = scroll_x + dt*scroll_speed
-	elseif self:key_is_down(input.event_codes.KEY_D) then
-		scroll_x = scroll_x - dt*scroll_speed
-	end
+	update_bullets(dt)
 	
+	if player.y > height+player.height then
+		self:change_stage("menu")
+	end
+	print("\n\n\n#player.bullets:" .. #bullets .. "     ")
 end
 
 -- called when the image is about to be drawn with the output drawbuffer
@@ -107,6 +180,9 @@ function game:draw(db)
 	-- draw player
 	draw_player(db)
 	
+	-- draw bullets
+	draw_bullets(db)
+	
 	-- draw ui ontop
 	font:draw_string(db, (" FPS: %.3f "):format(fps), 0, 0)
 end
@@ -115,6 +191,7 @@ end
 
 function game:init()
 	font = self:load_font("cga8")
+	local level = require("level")
 	
 	engine = self
 	
@@ -122,19 +199,19 @@ function game:init()
 	height = self.config.output.height
 	
 	player = {
-		x = 0,
-		y = 0,
+		x = level.spawn_x,
+		y = level.spawn_y,
 		width = 8,
 		height = 24,
-		velocity_y = 20,
-		velocity_x = 20,
-		speed_x = 20,
-		current_db = nil,
-		state = "standing_right",
+		can_shoot = true,
+		velocity_y = level.spawn_velocity_x,
+		velocity_x = level.spawn_velocity_y,
+		speed_x = 40,
+		dir = "right",
 		jump_height = 40,
 		gravity = 50,
-		friction_air = 0.99999,
-		friction_ground = 0.9,
+		friction_air = 0.99,
+		friction_ground = 0.75,
 		drawbuffers = {
 			standing_left = {
 				self:load_img("char_standing_left.bmp")
@@ -163,7 +240,7 @@ function game:init()
 		}
 	}
 	
-	local level = require("level")
+	
 	world = self:new_world(level, player)
 	
 	-- todo: build asset loader
@@ -179,7 +256,6 @@ function game:init()
 	self:apply_transparency_color(player.drawbuffers.walking_right[1], 255,255,255)
 	player.drawbuffers.walking_right[1] = self:crop(player.drawbuffers.walking_right[1], 12,6, 8,24)
 	
-	print("loaded game stage!")
 end
 
 
